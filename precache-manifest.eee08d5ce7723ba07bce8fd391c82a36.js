132cdacba09e9b3a42f6e1002dc35a2d13988120
Seong-ho Bae <grulswizzi@gmail.com>
2019-09-27 03:45:28 +0900

    Updates

---
+++ precache-manifest.eee08d5ce7723ba07bce8fd391c82a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6b715cf9660ccb5ed647fd83bcb6b2",
+    "url": "/movinmovin/index.html"
+  },
+  {
+    "revision": "f43021198c96288b419c",
+    "url": "/movinmovin/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "00b009d2ddeaa0e47b9b",
+    "url": "/movinmovin/static/js/2.201f37b5.chunk.js"
+  },
+  {
+    "revision": "f43021198c96288b419c",
+    "url": "/movinmovin/static/js/main.1060640e.chunk.js"
+  },
+  {
+    "revision": "95f470cf027d0fffa8aa",
+    "url": "/movinmovin/static/js/runtime-main.f37d016d.js"
+  }
+]);
